2f5f414607ecd605efa3871fb6a0acaaf43f919a
ALW-N <officialalwintomy@gmail.com>
2024-01-21 18:03:05 +0530

    Commit-1

---
+++ Travel Management System/src/travel/management/system/Splash.java
@@ -0,0 +1,16 @@
+package travel.management.system;
+
+import javax.swing.*;
+
+public class Splash extends JFrame {
+
+    Splash() {
+        setSize(1200, 600);
+        setLocation(200,100);
+        setVisible(true);
+    }
+
+    public static void main(String[] arsg) {
+        new Splash();
+    }
+}

c8e2771235de56fcb836052790ac408f1e08071c
ALW-N <officialalwintomy@gmail.com>
2024-01-24 07:42:04 +0530

    commit 3

--- Travel Management System/src/travel/management/system/Splash.java
+++ Travel Management System/src/travel/management/system/Splash.java
@@ -1,35 +1,48 @@
 package travel.management.system;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Splash extends JFrame {
-    
-    Splash() {
-//        setSize(1200, 600);
-//        setLocation(200,100);
-        
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/splash.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1200, 600, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);        
-        JLabel image = new JLabel(i3);
-        add(image);
-        
-        setVisible(true);
-    }
-    
-    public static void main(String[] arsg) {
-        Splash frame = new Splash();
-        int x = 1;
-        for (int i = 1; i <= 500; x += 7, i += 6) {
-            frame.setLocation(i, 1);
-            frame.setSize(x * i, i);
-            try {
+public class Splash {
+    public static void main(String[] args){
+        SplashFrame f1 = new SplashFrame();
+        f1.setVisible(true);
+        int i;
+        int x=1;
+        for(i=2; i<=600; i+=10, x+=7){
+            f1.setLocation(900 - ((i+x)/2), 500 - (i/2));
+            f1.setSize(i+x,i);
+            try{
                 Thread.sleep(10);
-            } catch (Exception e){
-                
-            }
+            }catch(Exception e){}
+        }
+        
+    }
+}
+class SplashFrame extends JFrame implements Runnable{
+    Thread t1;
+    SplashFrame(){
+        setLayout(new FlowLayout());
+        ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("Travel/Management/System/icons/splash.jpg"));
+        Image i1 = c1.getImage().getScaledInstance(1030, 750,Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i1);
+        
+        JLabel l1 = new JLabel(i2);
+        add(l1);
+        setUndecorated(true);
+        t1 = new Thread(this);
+        t1.start();
+    }
+    public void run(){
+        try{
+            Thread.sleep(7000);
+            this.setVisible(false);
             
+            Login l = new Login();
+            l.setVisible(true);
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
